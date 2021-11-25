7dea2cfd01c79f070962dcbb034c34776245de69
jibenliu <jibenliu@126.com>
2021-11-25 18:15:59 +0800

    微调

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createMemoryHistory, RouteRecordRaw } from 'vue-router'
+import {createMemoryHistory, createRouter, RouteRecordRaw} from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Login from '../components/Login.vue'
@@ -6,40 +6,43 @@ import Register from '../components/Register.vue'
 import Upload from '../components/Upload.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    component: About
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/upload',
-    name: 'Upload',
-    component: Upload
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: {
+            title: "首页",
+        }
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        component: About
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/upload',
+        name: 'Upload',
+        component: Upload
+    }
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
-  routes
+    history: createMemoryHistory(),
+    routes
 })
 
 export default router
